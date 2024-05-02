fc9458f5ee399d4dffabf85e0e5dbbb02005149f
Bro Rojame <brorojame2030@gmail.com>
2024-05-02 11:08:34 +0700

    add_kanji

--- java/p1/a.java
+++ java/p1/a.java
@@ -1,9 +1,9 @@
 class main{
     public static void main(String[] args){
        for (int i = 1;i <=10;i++){
-           while (true){
+
                System.out.printf("%s", "hi");
-           }
+           
         //    System.out.println(i);
        }
     }
